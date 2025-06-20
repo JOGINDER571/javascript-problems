0306f3b1cc5020304344e7f6046f9159ea5a6ea5
JOGINDER571 <snshrma7@gmail.com>
2025-06-20 06:47:32 +0530

    added currying question

---
+++ Questions/currying.js
@@ -0,0 +1,20 @@
+// In currying , we return a function for each function invoked which accept the next arguments inline , with the help of currying we can transform a function with multiple arguments into sequence of nested function.
+
+function sum(...args) {
+  let storage = [...args];
+  function currying(...args) {
+    if (args.length === 0) {
+      return storage.reduce((a, b) => a + b, 0);
+    }
+
+    storage = [...storage, ...args];
+    return currying;
+  }
+  currying.valueOf = function () {
+    return storage.reduce((a, b) => a + b, 0);
+  };
+  currying.value = currying.valueOf;
+  return currying;
+}
+
+console.log(sum(1)(2)(6)());
